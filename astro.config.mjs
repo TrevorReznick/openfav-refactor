c61d60e08216b6aa36bd6b857621ecdad20b3b51
Tuo Nome <double.facessss@gmail.com>
2024-10-03 18:30:50 +0200

    OPF-3 testing new login

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,32 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
 import tailwind from '@astrojs/tailwind'
-
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-
   integrations: [
-    tailwind()
+    tailwind(),
+    icon({
+      include: {
+        tabler: ['*'],
+        'flat-color-icons': [
+          'template',
+          'bookmark',
+          'gallery',
+          'approval',
+          'document',
+          'advertising',
+          'currency-exchange',
+          'voice-presentation',
+          'business-contact',
+          'database',
+          'workflow'
+        ],
+      },
+    })
   ],
-
   adapter: vercel()
 })
