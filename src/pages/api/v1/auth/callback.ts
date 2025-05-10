fb2904a50d9afdee0b27de08e4910fbfff77006e
joseph.k <double.facessss@gmail.com>
2025-05-10 09:53:33 +0200

    OPF-102 fixing provider redirect

--- src/pages/api/v1/auth/callback.ts
+++ src/pages/api/v1/auth/callback.ts
@@ -28,5 +28,5 @@ export const GET: APIRoute = async ({ url, cookies, redirect }) => {
     secure: true,
     httpOnly: true,
   })
-  return redirect('/build/page')
+  return redirect('/test/main')
 }
