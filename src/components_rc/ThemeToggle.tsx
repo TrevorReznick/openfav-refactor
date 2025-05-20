775ec60cf0994a74e25b580e07a52f00bdb2b582
joseph.k <double.facessss@gmail.com>
2025-05-20 17:07:36 +0000

    OPF-115 theme bar added

---
+++ src/components_rc/ThemeToggle.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react'
+import { Moon, Sun } from 'lucide-react'
+import { useStore } from '@nanostores/react'
+
+import { Button } from '@/components_rc/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components_rc/ui/dropdown-menu'
+import { themeStore } from '@/react/providers/themeProvider'
+
+export function ThemeToggle() {
+  const { theme } = useStore(themeStore)
+
+  const setTheme = (theme: 'light' | 'dark' | 'system') => {
+    const root = window.document.documentElement
+    root.classList.remove('light', 'dark')
+
+    if (theme === 'system') {
+      const systemTheme = window.matchMedia('(prefers-color-scheme: dark)')
+        .matches
+        ? 'dark'
+        : 'light'
+
+      root.classList.add(systemTheme)
+      themeStore.set({ ...themeStore.get(), theme, systemTheme })
+    } else {
+      root.classList.add(theme)
+      themeStore.set({ ...themeStore.get(), theme })
+    }
+
+    localStorage.setItem('ui-theme', theme)
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme('light')}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('system')}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
