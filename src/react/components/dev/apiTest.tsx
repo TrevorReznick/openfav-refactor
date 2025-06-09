7e87db003b2b2361c6826f8e6a2c23c7b5b8eeed
joseph.k <double.facessss@gmail.com>
2025-06-09 19:08:01 +0200

    OPF-112 restore all tests

--- src/react/components/dev/apiTest.tsx
+++ src/react/components/dev/apiTest.tsx
@@ -94,7 +94,7 @@ const BasicApiTest = () => {
 
     try {
 
-      /*const _sites = await sites.getAll(); // Chiama la funzione definita in apiClient
+      const _sites = await sites.getAll(); // Chiama la funzione definita in apiClient
       console.log('✅ Risultato della chiamata:', _sites)
       const _sites_by_id = await sites.getOne(156); // Chiama la funzione definita in apiClient
       console.log('✅ Risultato della chiamata:', _sites_by_id)
@@ -113,7 +113,6 @@ const BasicApiTest = () => {
       console.log('✅ Risultato della chiamata:', _delete_event)
       const _sites_categories = await sites.joinCategories(); // Chiama la funzione definita in apiClient
       console.log('✅ Risultato della chiamata:', _sites_categories)
-      */
       const _lists = await lists.getAll(); // Chiama la funzione definita in apiClient
       console.log('✅ Risultato della chiamata:', _lists)
       const _lists_by_userId = await lists.getListsByUserId(userId); // Chiama la funzione definita in apiClient
