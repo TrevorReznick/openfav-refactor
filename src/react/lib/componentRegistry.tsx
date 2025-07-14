7fa88e54f47f64b69fa1b63e2be2e6063a88e620
default2368 <default.mcp@gmail.com>
2025-07-14 18:35:52 +0200

    OPF-3 removing not base pages and component

--- src/react/lib/componentRegistry.tsx
+++ src/react/lib/componentRegistry.tsx
@@ -1,48 +1,6 @@
 import { componentLib } from '@/react/lib/componentLib'
 
 componentLib
-    .register('login', {
-        import: () => import('~/react/components/auth/Auth'),
-        metadata: {
-            description: 'Auth Form Component',
-            group: 'auth'
-        }
-    })
-    .register('api-test', {
-        import: () => import('~/react/components/examples/ApiTestComponent'),
-        metadata: {
-            description: 'API Test Console',
-            group: 'debug'
-        }
-    })
-    .register('api-test-V0', {
-        import: () => import('~/react/components/examples/ApiTestComponentV0'),
-        metadata: {
-            description: 'API Test Console',
-            group: 'debug'
-        }
-    })
-    .register('debug-api', {
-        import: () => import('~/react/components/debug/ApiTest'),
-        metadata: {
-            description: 'API Test Console',
-            group: 'debug'
-        }
-    })
-    .register('debug-auth', {
-        import: () => import('~/react/components/debug/DebugAuth'),
-        metadata: {
-            description: 'Auth Form Component',
-            group: 'auth'
-        }
-    })
-    .register('debug-session', {
-        import: () => import('~/react/components/debug/DebugSession'),
-        metadata: {
-            description: 'Session Debug Component',
-            group: 'debug'
-        }
-    })
     .register('posts', {
         import: () => import('~/react/components/examples/PostsComponent'),
         metadata: {
